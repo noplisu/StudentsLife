a25e8ad38810ba9d9192c5d7dd53b2df234b07af
krzysztofchadynka <kchadynka@o2.pl>
2014-10-26 14:37:13 +0100

    Create LookAtObject script

---
+++ Assets/Scripts/LookAtObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtObject : MonoBehaviour 
+{
+	Camera cam;
+
+	// Use this for initialization
+	void Start () 
+	{
+		cam = Camera.main.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//Debug.Log(cam.sc	
+	}
+}
