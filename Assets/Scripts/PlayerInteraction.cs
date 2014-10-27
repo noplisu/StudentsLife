ae49e988fdbc3dca2510944fa8fbfb68f613e587
krzysztofchadynka <kchadynka@o2.pl>
2014-10-27 19:28:11 +0100

    View at - Refactoring code

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -17,26 +17,26 @@ public class PlayerInteraction : MonoBehaviour {
 	void FixedUpdate () {
 		List<ObjectInteraction> facingObjects = new List<ObjectInteraction>(); 
 
-		for (int i = 0; i < interactions.Length; i++) 
+		foreach (ObjectInteraction obj in interactions) 
 		{
-			Vector3 direction = interactions[i].transform.position - transform.position;
+			Vector3 direction = obj.transform.position - transform.position;
 			float angle = Vector3.Angle(direction, transform.forward);
-
+			
 			if (angle < maxAngle)
 			{
-				facingObjects.Add(interactions[i]);
+				facingObjects.Add(obj);
 			}
-		}
+		}	
 
 		if (facingObjects.Count > 0)
 		{
 			ObjectInteraction highlightedObject = facingObjects[0];
 
-			for (int i = 1; i < facingObjects.Count - 1; i++) 
+			foreach(ObjectInteraction obj in facingObjects) 
 			{
-				if (facingObjects[i].Magnitude() < highlightedObject.Magnitude())
+				if (obj.Magnitude() < highlightedObject.Magnitude())
 				{
-					highlightedObject = facingObjects[i];
+					highlightedObject = obj;
 				}
 			}
 			
