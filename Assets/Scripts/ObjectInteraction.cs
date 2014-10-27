c39f52bb2b097b8410afc727e91f19bcf7ff9a69
krzysztofchadynka <kchadynka@o2.pl>
2014-10-27 17:48:24 +0100

    Create scripts: PlayerInteraction and ObjectInteraction

---
+++ Assets/Scripts/ObjectInteraction.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectInteraction : MonoBehaviour {
+
+	GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public float Magnitude()
+	{
+		Vector3 distance = transform.position - player.transform.position;
+
+		return distance.magnitude;
+	}
+
+	public void Highlight()
+	{
+		print (gameObject);
+	}
+}
---
+++ Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerInteraction : MonoBehaviour {
+
+	ObjectInteraction[] interactions;
+	public float maxAngle = 60f;
+	public float maxDistance = 4.5f;
+
+	// Use this for initialization
+	void Start () {
+		interactions = FindObjectsOfType<ObjectInteraction> ();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		List<ObjectInteraction> facingObjects = new List<ObjectInteraction>(); 
+
+		for (int i = 0; i < interactions.Length; i++) 
+		{
+			Vector3 direction = interactions[i].transform.position - transform.position;
+			float angle = Vector3.Angle(direction, transform.forward);
+
+			if (angle < maxAngle)
+			{
+				facingObjects.Add(interactions[i]);
+			}
+		}
+
+		if (facingObjects.Count > 0)
+		{
+			ObjectInteraction highlightedObject = facingObjects[0];
+
+			for (int i = 1; i < facingObjects.Count - 1; i++) 
+			{
+				if (facingObjects[i].Magnitude() < highlightedObject.Magnitude())
+				{
+					highlightedObject = facingObjects[i];
+				}
+			}
+			
+			if (highlightedObject.Magnitude () < maxDistance)
+				highlightedObject.Highlight ();
+		}
+	}
+}
