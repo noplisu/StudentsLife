3d5ae69d23dcf330dda5fc81285e49713b935214
pfitzner <patrykfitzner@gmail.com>
2014-11-22 21:39:27 +0100

    SliderManager
    :)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,9 +11,7 @@ public class Player : MonoBehaviour {
 	private Color green, blue, yellow, red;
 
 	void Start () {
-		setColors ();
 		setStartSliders ();
-		print ("Student entertainment: " + this.entertainment);
 	}
 
 	// Adding money to a player from: work, parents etc.
@@ -31,18 +29,14 @@ public class Player : MonoBehaviour {
 	private float needPassage(float param, float hours, float percent, Slider slider)
 	{
 		param -= (hours * percent);
-		print ("Before align: " + param);
 		align (ref param);
-		print ("After align: " + param);
 		slider.value = percentValue (param);
-		changeSliderColor (slider, param);
 		return param;
 	}
 
 	public void ResetStudy(){
 		this.study = 0.0f;
 		studySlider.value = this.study;
-		changeSliderColor (studySlider, this.study);
 	}
 
 	public void Change(ref float param, float value, ref Slider slider)
@@ -50,7 +44,6 @@ public class Player : MonoBehaviour {
 		param += value;
 		align (ref param);
 		slider.value = percentValue (param);
-		changeSliderColor (slider, param);
 	}
 
 	private void align(ref float number)
@@ -59,29 +52,6 @@ public class Player : MonoBehaviour {
 		if (number < 0.0f)number = 0.0f;
 	}
 	
-	private Color getColor(float param)
-	{
-		if (param <= 20.0f) return red;
-		if (param <= 50.0f) return yellow;
-		if (param <= 80.0f) return blue;
-		else return green;
-	}
-
-	private Color parseColor(int r, int g, int b)
-	{
-		float rF = r / 255.0f;
-		float gF = g / 255.0f;
-		float bF = b / 255.0f;
-		return new Color (rF, gF, bF);
-	}
-
-	private void changeSliderColor(Slider slider, float param)
-	{
-		Image img = slider.fillRect.GetComponent<Image>(); //slider.GetComponentInChildren<Image> ();
-		img.color = getColor (param);
-		print (img.color);
-	}
-
 	private float percentValue(float value)
 	{
 		return value / 100.0f;
@@ -90,23 +60,8 @@ public class Player : MonoBehaviour {
 	private void setStartSliders()
 	{
 		energySlider.value = percentValue (this.energy);
-		changeSliderColor (energySlider, this.energy);
-
 		foodSlider.value = percentValue (this.food);
-		changeSliderColor (foodSlider, this.food);
-
 		entertainmentSlider.value = percentValue (this.entertainment);
-		changeSliderColor (entertainmentSlider, this.entertainment);
-
 		studySlider.value = percentValue (this.study);
-		changeSliderColor (studySlider, this.study);
-	}
-
-	private void setColors()
-	{
-		green = parseColor (64,126,41);
-		blue = parseColor (12, 56, 121);
-		yellow = parseColor (225,189,36);
-		red = parseColor (188,0,0); 
 	}
 }
---
+++ Assets/Scripts/SliderManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SliderManager : MonoBehaviour {
+
+	private Color green, blue, yellow, red;
+	private Slider slider;
+
+	void Start () 
+	{
+		slider =  gameObject.GetComponent<Slider> ();
+		setColors ();
+		changeSliderColor (slider.value);
+	}
+
+	public void OnValueChange(float val)
+	{
+		print (val);
+		slider.value = val;
+		changeSliderColor (slider.value);
+	}
+
+	private Color getColor(float param)
+	{
+		if (param <= 0.2f) return red;
+		if (param <= 0.5f) return yellow;
+		if (param <= 0.8f) return blue;
+		else return green;
+	}
+	
+	private void changeSliderColor(float param)
+	{
+		Image img = slider.fillRect.GetComponent<Image>();
+		img.color = getColor (param);
+	}
+	
+	private Color parseColor(int r, int g, int b)
+	{
+		float rF = r / 255.0f;
+		float gF = g / 255.0f;
+		float bF = b / 255.0f;
+		return new Color (rF, gF, bF);
+	}
+
+	private void setColors()
+	{
+		green = parseColor (64,126,41);
+		blue = parseColor (12, 56, 121);
+		yellow = parseColor (225,189,36);
+		red = parseColor (188,0,0); 
+	}
+}
