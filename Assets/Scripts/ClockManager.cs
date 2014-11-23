2473fb2ce50a5b07228a9871b71f978f325bcda4
glisowski <lisux1991@tlen.pl>
2014-11-23 01:18:13 +0100

    Refactoring TimeManager, created PocketManager

--- Assets/Scripts/ClockManager.cs
+++ Assets/Scripts/ClockManager.cs
@@ -8,14 +8,17 @@ public class ClockManager : MonoBehaviour {
 
 	string[] Days = { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" };
 
-	int startHour = 0;
-	int startMinute = 0;
+	int startHour = 6;
+	int startMinute = 30;
+	int startDayOfWeek = 5;
+	float total_time;
 
 	public Text[] clocks;
 
 	void Start()
 	{
 		time = GetComponent<TimeManager> ();
+		total_time = time.HoursRemaining();
 	}
 
 	void FixedUpdate()
@@ -28,33 +31,31 @@ public class ClockManager : MonoBehaviour {
 
 	public int Hour()
 	{
-		return 23 - Mathf.FloorToInt (time.HoursRemaining() % 24);
+		float hoursPassed = total_time - time.HoursRemaining ();
+		return Mathf.FloorToInt ((startHour + hoursPassed) % 24);
 	}
 
 	public int Minutes()
 	{
-		float hours = time.HoursRemaining ();
-		float hourFraction = hours - Mathf.Floor(hours);
-		return 59 - Mathf.FloorToInt (hourFraction * 60);
+		float hoursPassed = total_time - time.HoursRemaining ();
+		float hourFraction = startMinute + (hoursPassed - Mathf.Floor(hoursPassed)) * 60;
+		return Mathf.FloorToInt (hourFraction % 60);
 	}
 
 	public string Time()
 	{
 		float minutes = Minutes();
-		string minutesString;
+		string minutesString = "";
 		if (minutes < 10) 
 		{
-			minutesString = "0" + minutes.ToString ();
+			minutesString += "0";
 		}
-		else 
-		{
-			minutesString = minutes.ToString ();
-		}
+		minutesString += minutes.ToString ();
 		return string.Format("{0}:{1}", Hour(), minutesString);
 	}
 
 	public string DayOfWeek()
 	{
-		return Days[time.DayOfWeek()];
+		return Days[(startDayOfWeek + time.DayOfWeek()) % 7];
 	}
 }
---
+++ Assets/Scripts/PocketManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PocketManager : MonoBehaviour {
+
+	public float ParentMoney = 168f;
+
+	TimeManager time;
+	Player player;
+	int previousDayOfWeek;
+	int previousSemester;
+
+	int mark = 4;
+
+	// Use this for initialization
+	void Start () {
+		time = FindObjectOfType<TimeManager> ();
+		player = FindObjectOfType<Player> ();
+		previousDayOfWeek = time.DayOfWeek();
+		previousSemester = time.CurrentSemester();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int dayOfWeek = time.DayOfWeek();
+		int semester = time.CurrentSemester();
+		if (previousDayOfWeek == 6 && dayOfWeek == 0)
+			player.changeMoney (ParentMoney);
+		if (previousSemester < semester && mark >= 4)
+			player.changeMoney (mark * 100);
+		previousSemester = semester;
+		previousDayOfWeek = dayOfWeek;
+	}
+}
