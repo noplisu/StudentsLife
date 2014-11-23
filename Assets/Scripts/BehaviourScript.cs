beee65c5d43ec7d128131097fe41d7e7b66c6e46
krzysztofchadynka <kchadynka@o2.pl>
2014-11-23 08:21:32 +0100

    Create Main Menu prototype

---
+++ Assets/Scripts/BehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BehaviourScript : MonoBehaviour
+{
+	void Start () 
+	{
+	
+	}
+	
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.Escape))
+			Application.LoadLevel ("main");
+	}
+
+	void OnGUI()
+	{
+
+	}
+}
---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour 
+{
+	void Start () 
+	{
+	
+	}
+
+	void Update () 
+	{
+		
+	}
+
+	public void Level()
+	{
+		Application.LoadLevel ("krzysiek-prefab");
+	}
+
+	public void Quit()
+	{
+
+	}
+}
