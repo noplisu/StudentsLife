97c7ba8028ea16e34b48e4d5126d4c1fbc04d0e3
krzysztofchadynka <kchadynka@o2.pl>
2014-12-16 22:38:35 +0100

    add save script

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -3,39 +3,38 @@ using System.Collections;
 using System.Runtime.Serialization.Formatters.Binary; 
 using System.IO;
 
-public static class GameState{
-
+public class GameState : MonoBehaviour 
+{
 	public static Player player;
 	public static TimeManager tm;
-	 
 
-	public static void Save()
+	public void Save()
 	{
 		player = Player.current;
 		tm = TimeManager.current;
 		BinaryFormatter bf = new BinaryFormatter();
-
+		
 		FileStream file = File.Create (Application.persistentDataPath + "/savedPlayer.gd"); 
-		bf.Serialize(file, GameState.player);
+		bf.Serialize(file, player);
 		file.Close();
-
+		
 		FileStream file2 = File.Create (Application.persistentDataPath + "/savedTime.gd"); 
-		bf.Serialize(file2, GameState.tm);
+		bf.Serialize(file2, tm);
 		file2.Close();
 	}
-
-	public static void Load()
+	
+	public void Load()
 	{
-		if(File.Exists(Application.persistentDataPath + "/savedPlayer.gd")) {
+		if (File.Exists(Application.persistentDataPath + "/savedPlayer.gd"))
+		{
 			BinaryFormatter bf = new BinaryFormatter();
 			FileStream file = File.Open(Application.persistentDataPath + "/savedPlayer.gd", FileMode.Open);
-			GameState.player = (Player)bf.Deserialize(file);
+			player = (Player)bf.Deserialize(file);
 			file.Close();
-
+			
 			FileStream file2 = File.Open(Application.persistentDataPath + "/savedTime.gd", FileMode.Open);
-			GameState.tm = (TimeManager)bf.Deserialize(file2);
+			tm = (TimeManager)bf.Deserialize(file2);
 			file2.Close();
+		}
 	}
-
-
 }
