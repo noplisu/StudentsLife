0bb0b7c974bc702a60054aa171de61d09505bc3f
krzysztofchadynka <kchadynka@o2.pl>
2014-12-16 21:39:45 +0100

    Add scripts to upgrade

--- Assets/Scripts/CanvasCloser.cs
+++ Assets/Scripts/CanvasCloser.cs
@@ -1,17 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CanvasCloser : MonoBehaviour {
-
+public class CanvasCloser : MonoBehaviour 
+{
 	GameObject gameO;
-
+	
 	void Start()
 	{
 		gameO = gameObject;
 	}
-
+	
 	public void Hide()
 	{
 		gameO.SetActive (false);
+		Time.timeScale = 1.0f;
 	}
 }
